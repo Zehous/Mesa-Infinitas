545350563c4e538afa90e556e7c60166cc2b64f7
Zehous <zehousnorton@gmail.com>
2021-11-30 14:25:16 -0300

    master

--- index.js
+++ index.js
@@ -19,5 +19,4 @@ client.on("message", async msg => {
     require('./Modulos/Commandos').Run(msg, cmd, args);
 });
 
-//client.login(process.env.TokenDiscord);
-client.login("NzU5MDc5NjgzODgxOTU5NDg1.X24SCw.vUvTInZJXBPnBy8Q_pSztMjXt0g");
+client.login(process.env.TokenDiscord);
