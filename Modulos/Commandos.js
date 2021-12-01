9878b96e73e877a7a1d6349e04c74061da6a59ea
Zehous <zehousnorton@gmail.com>
2021-12-01 16:30:44 -0300

    master

--- Modulos/Commandos.js
+++ Modulos/Commandos.js
@@ -116,11 +116,7 @@ async function CriarMesa(Context, Name, Msg)
 
     var Config = [
         { id: Context.guild.id, deny: ['VIEW_CHANNEL']}, 
-        { id: Role.id, allow: ['VIEW_CHANNEL']}, 
-        { id: Role.id, allow: ['READ_MESSAGE_HISTORY']}, 
-        { id: Role.id, allow: ['SEND_MESSAGES']}, 
-        { id: Role.id, allow: ['CONNECT']}, 
-        { id: Role.id, allow: ['SPEAK']} 
+        { id: Role.id, allow: ['VIEW_CHANNEL', 'READ_MESSAGE_HISTORY', 'SEND_MESSAGES', 'CONNECT', 'SPEAK']}
     ]
 
     var Category = await Utility.createCategory(Context, `.${Name}`, Config);
