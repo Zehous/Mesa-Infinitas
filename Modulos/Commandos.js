60613b998987711c85f8f71ded088f21ea0c6b7e
Zehous <zehousnorton@gmail.com>
2021-12-01 15:54:00 -0300

    master

--- Modulos/Commandos.js
+++ Modulos/Commandos.js
@@ -9,7 +9,7 @@ module.exports = {
                 return;
             }
 
-            if (Args[0].toLowerCase() === "criar")
+            if (Args[0].toLowerCase() === "criar" || Args[0].toLowerCase() === "create")
             {
                 if (Args.length == 1)
                 {
